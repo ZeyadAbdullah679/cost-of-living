08eeb56630307c2f6e508734bb53c0ecf246a4cb
Royaa <mennaibrahim4691@gmail.com>
2023-02-26 07:03:56 +0200

    delete unwanted functions

--- src/main/kotlin/interactor/Consumption.kt
+++ src/main/kotlin/interactor/Consumption.kt
@@ -5,48 +5,33 @@ import model.CityEntity
 
 class Consumption (private val dataSource: CostOfLivingDataSource){
 
-    fun execute(): String {
-         val x:MutableMap<Float,MutableList<String>> = mutableMapOf(Pair((0).toFloat(), mutableListOf()))
-         val cities=dataSource
+    fun execute(): CityEntity {
+         val x: MutableMap<Float,MutableList<CityEntity>> = mutableMapOf()
+         this.dataSource
             .getAllCitiesData()
-            .filter(::excludeNullValue)
-            .sortedBy{it.realEstatesPrices.apartment3BedroomsOutsideOfCentre}
-        cities.forEach {
-             val servicesCoast= servicesCoast(it)
-             val remainder= remainder(it,servicesCoast)
-             if(isSuitable(servicesCoast,remainder)){
-                 if (x.contains(remainder)) x[remainder]?.add(it.cityName)
-                 else x[remainder] = mutableListOf(it.cityName)
+            .filter(::excludeNullOrZeroValue)
+            .forEach {
+             val remainder= remainder(it)
+             if(remainder>(250).toFloat()){
+                 if (x.contains(remainder)) x[remainder]?.add(it)
+                 else x[remainder] = mutableListOf(it)
              }
          }
         return x.toSortedMap()
             .values
-            .last()
-            .toString()
+            .last()[0]
     }
 }
-private fun excludeNullValue(city: CityEntity): Boolean {
-    return city.averageMonthlyNetSalaryAfterTax != null
-            && city.dataQuality
-            &&city.realEstatesPrices.apartment3BedroomsOutsideOfCentre != null
-            &&city.foodPrices.riceWhite1kg != null
-            &&city.foodPrices.chickenFillets1kg != null
-            &&city.foodPrices.beefRound1kgOrEquivalentBackLegRedMeat != null
-            &&city.foodPrices.localCheese1kg != null
-            &&city.foodPrices.loafOfFreshWhiteBread500g != null
-            &&city.servicesPrices.basicElectricityHeatingCoolingWaterGarbageFor85m2Apartment != null
-            &&city.servicesPrices.internet60MbpsOrMoreUnlimitedDataCableAdsl != null
-            &&city.servicesPrices.internationalPrimarySchoolYearlyForOneChild != null
-            &&city.servicesPrices.oneMinOfPrepaidMobileTariffLocalNoDiscountsOrPlans != null
+private fun excludeNullOrZeroValue(city: CityEntity): Boolean {
+    return !(city.averageMonthlyNetSalaryAfterTax == null
+            || city.realEstatesPrices.apartment3BedroomsOutsideOfCentre == null
+            || city.foodPrices.riceWhite1kg == null
+            || city.foodPrices.chickenFillets1kg == null
+            || city.foodPrices.beefRound1kgOrEquivalentBackLegRedMeat == null
+            || city.foodPrices.localCheese1kg == null
+            || city.foodPrices.loafOfFreshWhiteBread500g == null)
 }
-
-private fun servicesCoast(city:CityEntity):Float{
-    return (city.servicesPrices.basicElectricityHeatingCoolingWaterGarbageFor85m2Apartment!!
-        +(city.servicesPrices.internet60MbpsOrMoreUnlimitedDataCableAdsl!!)
-        +(city.servicesPrices.internationalPrimarySchoolYearlyForOneChild!!/6)
-        +(city.servicesPrices.oneMinOfPrepaidMobileTariffLocalNoDiscountsOrPlans!!))
-    }
-private fun remainder(city:CityEntity,servicesCoast:Float):Float{
+private fun remainder(city:CityEntity):Float{
 
     return( city.averageMonthlyNetSalaryAfterTax!!*2
             -city.realEstatesPrices.apartment3BedroomsOutsideOfCentre!!
@@ -54,9 +39,5 @@ private fun remainder(city:CityEntity,servicesCoast:Float):Float{
             -city.foodPrices.chickenFillets1kg!!*10
             -city.foodPrices.beefRound1kgOrEquivalentBackLegRedMeat!!*4
             -city.foodPrices.localCheese1kg!!
-            -city.foodPrices.loafOfFreshWhiteBread500g!!*30
-            -servicesCoast)
+            -city.foodPrices.loafOfFreshWhiteBread500g!!*30)
     }
-private fun isSuitable(servicesCoast:Float,remainder:Float):Boolean{
-    return servicesCoast <(250).toFloat() && remainder>(0).toFloat()
-}
